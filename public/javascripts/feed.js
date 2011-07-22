d2ffc93e6bd8e3ac60624a152e074f2d53fcb0fc
Elliot Babchick <ebabchick@surfstream.tv>
2011-07-22 13:51:49 -0700

    express set up

---
+++ public/javascripts/feed.js
@@ -0,0 +1,155 @@
+var socket, ytplayer, currVideo = {}, djInfo, me;
+
+var playerLoaded = false;
+
+function configureDJButtons() {
+	$('#beDJ').show();
+	$('#quitDJ').hide();
+}
+
+$(document).ready(function () {
+  $('#fbData').hide();
+	$clientCounter = $("#client_count")
+	$video = $("#video")
+	$time = $("#time")
+	
+	configureDJButtons();
+ 	var videoLoaded = false;
+ 	
+	socket = io.connect();
+	socket.emit('getUserData', JSON.parse($('#fbData').text()));
+	$('#fbData').remove();
+	
+	socket.on('clientInfo', function(info) {
+		console.log("Received client info: "+JSON.stringify(info));
+		me = info;
+	});
+	
+	socket.on('djInfo', function(data) {
+		console.log("Received dj info: "+JSON.stringify(data));
+		djInfo = data;
+		$('#djCount').html(djInfo.length);
+		var index = djInfo.indexOf(me['socketId']);
+		
+		if(index >= 0) {	//client is dj
+			index += 1;
+			$('#djStatus').html("You are DJ number "+index);
+			$('#beDJ').hide();
+			$('#quitDJ').show();
+		} else {
+			configureDJButtons();
+		}
+	});
+	
+	socket.on('videoInfo', function(data) {
+		
+		console.log("Socket received video info");
+		currVideo.video = data.video;
+		currVideo.timeStart = data.time;
+		
+		if(playerLoaded) {
+			ytplayer.loadVideoById(currVideo.video, currVideo.time);
+		} else {
+			var params = { allowScriptAccess: "always" };
+			var atts = { id: "myytplayer"};
+			swfobject.embedSWF("http://www.youtube.com/apiplayer?&enablejsapi=1&playerapiid=ytplayer",
+		                       "videoPlayer", "640", "390", "8", null, null, params, atts);
+			playerLoaded = true;
+		}
+	}); 
+	
+	socket.on('clientUpdate', function(numClients) {
+		console.log("client has been updated: "+numClients);
+		$clientCounter.html(numClients);
+	});
+	
+	socket.on('refreshPlaylist', function(data) {
+		console.log("Refreshing playlist..."+data);
+		data = String(data);
+		videos = data.split(',');
+		if(videos.length > 0) {
+			var playlistHtml = '<ul>';
+			for(var v in videos)
+				playlistHtml += '<li>'+videos[v]+'</li>';
+			playlistHtml += '</li>';
+			$("#playlist").html(playlistHtml);
+		}
+	})
+});
+
+function onYouTubePlayerReady(playerId) {
+    ytplayer = document.getElementById('myytplayer');
+		ytplayer.loadVideoById(currVideo.video, currVideo.timeStart);
+    ytplayer.addEventListener('onStateChange', 'onytplayerStateChange');
+}
+
+function onytplayerStateChange(newState) {
+	$('#state').html("Player state: "+newState);
+	if(newState == 0 && currVideo.isLeader) { 
+		console.log("Video finished, broadcasting back to server");
+		socket.emit('videoFinished');
+		currVideo = {};
+	}
+}
+
+function addVideo() {
+	console.log("adding video to queue");
+	var videoToAdd = $('#videoEntry').val();
+	socket.emit('addVideoToQueue', { video: videoToAdd });
+}
+
+function searchYouTube() {
+  var query = $('#searchBox').val();
+  console.log("just searched for video: ");
+  console.log(query);
+  $.get("http://gdata.youtube.com/feeds/api/videos?max-results=5&alt=json&q=" + query, showMyVideos); 
+}
+
+function getRelatedVideos() {
+  
+}
+
+function showMyVideos(data) {
+  var feed = data.feed;
+  var entries = feed.entry || [];
+  var html = ['<ul class="videos">'];
+  var template = $('.videoResultTemplate');
+  var $list = $('#videoList');
+  for (var i = 0; i < entries.length; i++) {
+    var entry = entries[i];
+    var $item = template.clone().removeClass('videoResultTemplate');
+    console.log("in for statement3");
+    /*var videoResult = new VideoResult({
+      root: $item,
+      title: entry.title.$t,
+      image_src: entry.media$group.media$thumbnail[0].url,
+      videoUrl: entry.id.$t
+    })*/
+    $item.find('#videoTitle').text(entry.title.$t);
+    $item.find('#videoThumbnail').attr({
+      src: entry.media$group.media$thumbnail[0].url,
+      alt: 'Video blah'
+    });
+    $item.find('#addToPlaylist').text(entry.id.$t.substring(42));
+    $item.find('#addToPlaylist').bind('click', function (event) {
+      console.log(this.innerHTML);
+    });
+    console.log("in for statement4");
+    $list.append($item);
+    console.log("in for statement5");
+  }
+
+function becomeDJ() {
+	console.log("user "+me.user.id+ " aka "+me.user.name+ " wants to become a DJ");
+	if(djInfo.length < 4) {
+		console.log("requesting that user becomes a dj");
+		socket.emit('becomeDJ');
+	}
+}
+
+function quitDJ() {
+	if(djInfo.indexOf(me.socketId) < 0) return;
+	configureDJButtons();
+	console.log('Quitting DJ');
+	socket.emit('quitDJ');
+}
---
+++ server.js
@@ -0,0 +1,233 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express'),
+	http = require('http'), 
+	url = require('url'),
+	app = express.createServer(),
+	io = require('socket.io').listen(app),
+	redis = require('redis'),
+	redisClient = redis.createClient();
+	
+/* Custom Modules */
+var facebook = require('./facebook');
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser());
+  app.use(express.session({ secret: 'your secret here' }));
+  app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+
+// Routes
+
+facebook.connect(app, express);
+
+app.get('/*.(js|css)', function(req, res){
+  res.sendfile("./public"+req.url);
+});
+
+//keep track of the people in the room
+var djList = new Array();
+var currDjIndex = -1;
+var clients = new Array();
+var socketToUser = {};
+
+//related to the current video
+var currVideo = null;
+
+function sendDJInfo(socket) {
+	socket.emit('djInfo', djList);
+}
+
+function sendClientInfo(socket, fbInfo) {
+	fbInfo['socketId'] = socket.id;
+	socket.emit('clientInfo', fbInfo);
+}
+
+io.sockets.on('connection', function(socket) {
+	socket.on('getUserData', function(fbUser) {
+		console.log("Saving to redis...user id: "+fbUser.user.id);
+		//TODO: need to check and see if redis already has the id
+		redisClient.set('user:'+fbUser.user.id+':info', JSON.stringify(fbUser)); 
+		console.log('socket id: '+socket.id);
+		console.log('socketToUser[socket.id] = '+socketToUser[socket.id]);
+
+		console.log('about to push onto clients the user id '+fbUser.user.id);
+		clients.push(fbUser.user.id);
+		socketToUser[socket.id] = fbUser.user.id;
+		
+		announceClients();
+		sendClientInfo(socket, fbUser);
+		sendFullPlaylist(socket, fbUser.user.id);
+		sendDJInfo(socket);
+
+		addListeners(socket);
+	});
+	
+	if(currVideo) {
+		var timeIn = new Date();
+		var timeDiff = (timeIn.getTime() - currVideo.timeStart) / 1000; //time difference in seconds
+		socket.emit('videoInfo', { video: currVideo.video, time: Math.ceil(timeDiff) });
+	}
+});
+
+function addListeners(socket) {
+	socket.on('disconnect', function() { 
+		clientDisconnect(socketToUser[socket.id]);
+		removeFromDJ(socket.id);	
+		delete socketToUser[socket.id]; 
+	});
+	
+	socket.on('addVideoToQueue', function(data) {
+		var userId = socketToUser[socket.id];
+		console.log("user "+ userId +" wants to add video: "+data.video);
+		redisClient.rpush("user:"+userId+":queue", data.video, function(err,res) { sendFullPlaylist(socket, userId)});
+	});
+	
+	socket.on('becomeDJ', function() {
+		console.log('user '+socketToUser[socket.id]+' requesting to be DJ');
+		
+		if(djList.length < 4 && djList.indexOf(socket.id) < 0) {
+			console.log('user '+socketToUser[socket.id]+' is now a DJ');
+			djList.push(socket.id);
+			announceDJs(); 
+		}
+		if(djList.length == 1) {
+			currDJIndex = 0;			//eventually we'll want to replace this with a full DJ model
+			playVideoFromPlaylist(socket.id);
+		}
+	});
+	
+	socket.on('quitDJ', function() { 
+		console.log("Quit dj event called for socket" + socket.id);
+		removeFromDJ(socket.id) 
+	});
+}
+
+function getVideoDurationAndPlay(videoId) {
+	if(!videoId) {
+		console.log('Need another video to load! videoId is null or undefined: '+videoId);
+		playNextVideo();
+		return;
+	}
+	console.log("Video to play has id: "+videoId);
+	var options = { 
+		host: 'gdata.youtube.com',
+		port: 80,
+		path: '/feeds/api/videos/'+videoId+'?&alt=json',
+		method: 'GET'
+	};
+		
+	var req = http.request(options, function(res) {
+	  res.setEncoding('utf8');
+		var videoData = '';
+	  res.on('data', function (chunk) {
+			//console.log(chunk);
+	    videoData += chunk;
+	  });
+		res.on('end', function() {
+			console.log('Finished getting info for video '+videoId);
+			videoData = JSON.parse(videoData);
+			var videoDuration = videoData['entry']['media$group']['yt$duration']['seconds'];
+			console.log("Video length: "+videoDuration+" seconds");
+			announceVideo(videoId, videoDuration);
+		})
+	});
+		
+	req.on('error', function(e) {
+	  console.log('problem with request: ' + e.message);
+	});
+	req.end();
+}
+
+function playNextVideo() {
+	if(djList.length == 0) {
+		console.log("No DJs, stopping");
+		return;
+	}
+	currDJIndex = (currDJIndex + 1) % djList.length;
+	console.log('Playing next video, dj has index '+currDJIndex+' and is user '+socketToUser[djList[currDJIndex]]);
+	playVideoFromPlaylist(djList[currDJIndex]);
+}
+
+function announceVideo(videoId, videoDuration) {
+	currVideo.video = videoId;
+	currVideo.duration = videoDuration;
+	currVideo.timeStart = (new Date()).getTime();
+	setTimeout(function() { playNextVideo() }, videoDuration*1000);	//*1000 for milliseconds
+	io.sockets.emit('videoInfo', { video: videoId, time: 0 });
+}
+
+function playVideoFromPlaylist(socketId) {
+	var userId = socketToUser[socketId];
+	redisClient.lpop('user:'+userId+':queue', function(err, reply) {
+		if(err) {
+			console.log("error in getting the first video from playlist for "+userId+": "+err);
+			return;
+		}
+		currVideo = {};	//makes it non-null
+		currVideo.socketIdOfDj = socketId;
+		getVideoDurationAndPlay(reply)
+	});
+}
+
+function removeFromDJ(socketId) {
+	var index = djList.indexOf(socketId);
+	if(index != -1) djList.splice(index,1);
+	announceDJs();
+}
+
+function announceDJs() {
+	io.sockets.emit('djInfo', djList);
+}
+
+function sendFullPlaylist(socket, userId) {
+	redisClient.llen('user:'+userId+':queue', function(err, size) {
+		console.log('size of '+userId+"'s queue: "+size);
+		redisClient.lrange('user:'+userId+':queue', 0, size, function(err, reply) {
+			if(err) {
+				console.log("error in refreshing playlist for "+userId+": "+err);
+				return;
+			}
+			socket.emit("refreshPlaylist", reply); 
+		});
+	})	
+}
+
+function clientDisconnect(userId) {
+	console.log("userId "+userId+" is disconnecting");
+	var index = clients.indexOf(userId);
+	if(index != -1) clients.splice(index,1);
+	announceClients();
+}
+
+function announceClients() {
+	console.log("'announceClients' fired to all sockets, client count: "+clients.length);
+	io.sockets.emit('clientUpdate', clients.length);
+}
+
+app.listen(3000);
+
+// console.log("Starting timer...");
+// setTimeout(function() { console.log("10 seconds have passed")}, 10000);
+// console.log("Timer is active.");
+
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
