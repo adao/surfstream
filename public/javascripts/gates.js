1ef9bc4fafc19c55288788aaedb2a7d5e8b114d6
Elliot Babchick <ebabchick@surfstream.tv>
2011-07-30 17:36:40 -0700

    Latest work in progress for Anthony

---
+++ public/javascripts/gates.js
@@ -0,0 +1,389 @@
+$(function(){
+	
+	/**************/
+	/*** MODELS ***/
+	/**************/
+	
+	window.ChatMessageModel = Backbone.Model.extend({});
+	
+	window.PlayerModel = Backbone.Model.extend({
+		initialize: function () {
+			
+		}
+	});
+	
+	
+	window.RoomModel = Backbone.Model.extend({		
+		initialize: function () {
+			
+		}
+		
+	});
+	
+	window.SearchModel = Backbone.Model.extend({
+		initialize: function () {
+			
+		}
+	});
+	
+	window.SharingModel = Backbone.Model.extend({
+		initialize: function () {
+			
+		}
+	});
+	
+	
+	window.UserModel = Backbone.Model.extend({
+		defaults: {
+			is_main_user: false,			
+		}
+	
+		initialize: function () {
+			if (this.get("is_main_user")){
+				//FOR ANTHONY -- USE FB SDK HOPEFULLY TO PULL USER DATA HERE
+				
+				
+			}
+		}
+		
+	});
+	
+	window.VideoModel = Backbone.Model.extend({
+		initialize: function () {
+			
+		}
+		
+	});
+	
+	window.SocketManager = Backbone.Model.extend({
+		initialize: function () {
+			var socket, app;
+			socket = this.get("socket");
+			app = this.get("app");
+			if (!socket){
+				throw "No Socket Passed to SocketManager!";
+			} else {
+				console.log("Got socket. " + socket);
+			}
+			
+			if (!app){
+				throw "No App Passed to SocketManager!";
+			} else {
+				console.log("Got app. " + app);
+			}
+			
+			//Chat -- msg received
+			socket.on('message', function(msg) {
+				app.get("room").get("chat").add({user: msg.sender, message: msg.text});				
+			});
+			
+			
+			
+			/*
+			//Sends list of who is DJing
+			//Data is a js array of DJ Socket IDs
+			socket.on('dj:announceDJs', function(data) {
+				console.log("Received dj info: "+JSON.stringify(data));
+				djInfo = data;
+				$('#djCount').html(djInfo.length);
+				var index = djInfo.indexOf(me['socketId']);
+
+				if(index >= 0) {	//client is dj
+					index += 1;
+					$('#djStatus').html("You are DJ number "+index);
+					$('#beDJ').hide();
+					$('#quitDJ').show();
+				} else {
+					configureDJButtons();
+				}
+				
+			});
+			
+			socket.on('playlist:refresh', function(data) {
+
+				console.log("Socket received video info");
+				currVideo.video = data.video;
+				currVideo.timeStart = data.time;
+
+				if(playerLoaded) {
+					ytplayer.loadVideoById(currVideo.video, currVideo.time);
+				} else {
+					var params = { allowScriptAccess: "always" };
+					var atts = { id: "myytplayer"};
+					swfobject.embedSWF("http://www.youtube.com/apiplayer?version=3&enablejsapi=1&playerapiid=ytplayer",
+				                       "videoPlayer", "640", "390", "8", null, null, params, atts);
+					playerLoaded = true;
+				}
+			});
+			
+			socket.on('clientUpdate', function(numClients) {
+				console.log("client has been updated: "+numClients);
+				//$clientCounter.html(numClients);
+			});
+
+			socket.on('playlist:refresh', function(data) {
+				console.log("Refreshing playlist..."+data);
+				data = String(data);
+				videos = data.split(',');
+				if(videos.length > 0) {
+					var playlistHtml = '<ul>';
+					for(var v in videos)
+						playlistHtml += '<li>'+videos[v]+'</li>';
+					playlistHtml += '</li>';
+					$("#playlist").html(playlistHtml);
+				}
+			});
+			
+			socket.on('videoInfo', function(data) {
+
+				console.log("Socket received video info");
+				currVideo.video = data.video;
+				currVideo.timeStart = data.time;
+
+				if(playerLoaded) {
+					ytplayer.loadVideoById(currVideo.video, currVideo.time);
+				} else {
+					var params = { allowScriptAccess: "always" };
+					var atts = { id: "myytplayer"};
+					swfobject.embedSWF("http://www.youtube.com/apiplayer?version=3&enablejsapi=1&playerapiid=ytplayer",
+				                       "videoPlayer", "640", "390", "8", null, null, params, atts);
+					playerLoaded = true;
+				}
+			});
+			*/
+	
+		},
+		
+		
+		
+	});
+	
+	window.SurfStream = Backbone.Model.extend({
+		defaults: {
+			sharing: new SharingModel,			
+		}
+		
+		initialize: function (socket) {
+			
+			this.set({room: new RoomModel({
+														player: new PlayerModel,
+														chat: new ChatList, 
+														search: new SearchModel,
+														video: new VideoModel,
+														user: this.get("user")
+													}),
+								socket_manager: new SocketManager({
+														socket: socket, app: this
+													}),
+								user: new UserModel/*({is_main_user: true, fbid: $(#fb_user_init).html() })*/
+							});
+							
+		}
+	});
+	
+	/*FOR ANTHONY: MAKE SURE ON PAGE <div id="fb_user_init">userID</div>*/
+	
+	/************************/
+	/**COLLECTIONS (LISTS)**/
+	/***********************/
+	
+	window.ChatList = Backbone.Collection.extend({
+		model: ChatMessageModel,
+		
+		defaults: {
+			"clientId": 0
+		},
+		
+		initialize: function () {
+			
+		}
+	});
+	
+	window.FriendList = Backbone.Collection.extend({
+		model: UserModel,
+		
+		initialize: function () {
+			
+		}
+		
+	});
+	
+	/*** BEGIN: PLAYLIST AND SUBCLASSES ***/
+	
+	window.Playlist = Backbone.Collection.extend({
+		model: VideoModel,
+		
+		initialize: function () {
+			
+		}
+		
+	});
+	
+	window.PersonalHistoryList = window.Playlist.extend({
+		initialize: function () {
+			
+		}
+		
+	});
+	
+	
+	window.RoomHistoryList = window.Playlist.extend({
+		initialize: function () {
+			
+		}
+		
+	});
+	
+	window.SuggestionsList = window.Playlist.extend({
+		initialize: function () {
+			
+		}
+		
+	});
+	
+	/*** END: PLAYLIST AND SUBCLASSES ***/
+	
+	
+	window.RoomUsersList = Backbone.Collection.extend({
+		model: UserModel,
+		
+		initialize: function () {
+			
+		}
+	});
+	
+	/*NOT IMPLEMENTING THIS WEEKEND (7/30)*/
+	window.RoomList = Backbone.Collection.extend({
+		model: RoomModel,
+		
+		initialize: function () {
+			
+		}
+	});
+
+	
+	
+	/***************/
+	/*****VIEWS*****/
+	/***************/
+
+  window.TopBar = Backbone.View.extend({
+		initialize: function () {
+			
+		}
+		
+	});
+	
+	window.RoomInfo = Backbone.View.extend({
+		initialize: function () {
+			
+		}
+		
+	});
+	
+	window.ShareBar = Backbone.View.extend({
+		initialize: function () {
+			
+		}
+		
+	});
+
+	window.Tabs = Backbone.View.extend({
+		initialize: function () {
+			
+		}
+		
+	});
+	
+	window.SearchBar = Backbone.View.extend({
+		initialize: function () {
+			
+		}
+		
+	});
+	
+	window.VideoList = Backbone.View.extend({
+		initialize: function () {
+			
+		}
+		
+	});
+	
+	window.VideoCell = Backbone.View.extend({
+		initialize: function () {
+			
+		}
+		
+	});
+	
+	window.ChatList = Backbone.View.extend({
+		initialize: function () {
+			
+		}
+		
+	});
+	
+	window.ChatCell = Backbone.View.extend({
+		initialize: function () {
+			
+		}
+		
+	});
+	
+	//The Avatar + Seating Area
+	window.Theatre = Backbone.View.extend({
+		initialize: function () {
+			
+		}
+		
+	});
+	
+	//The Actual Video Player Presentation
+	window.Screen = Backbone.View.extend({
+		initialize: function () {
+			
+		}
+		
+	});
+	
+	window.Remote = Backbone.View.extend({
+		initialize: function () {
+			
+		}
+		
+	});
+	
+	window.Meter = Backbone.View.extend({
+		initialize: function () {
+			
+		}
+		
+	});
+	
+	window.Avatar = Backbone.View.extend({
+		initialize: function () {
+			
+		}
+		
+	});
+	
+	window.ProfilePage = Backbone.View.extend({
+		initialize: function () {
+			
+		}
+		
+	});
+	
+	window.SettingsView = Backbone.View.extend({
+		initialize: function () {
+			
+		}
+		
+	});
+
+	/* INITIALIZATION */
+	
+	socket_init = io.connect();
+	window.SurfStream = new SurfStream({socket: socket_init});
+	
+});
