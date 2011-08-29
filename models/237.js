8d49876852a767270242f6a2b9b3a06af6fce21b
Elliot Babchick <ebabchick@surfstream.tv>
2011-08-29 01:35:05 -0700

    valstream API tweaks

--- models/237.js
+++ models/237.js
@@ -80,6 +80,8 @@
 		
 		playVideo: function() {
 			var roomName = this.room.get('name');
+			var room = this.room;
+			var VAL = this;
 			console.log('['+roomName+'][VAL] playVideo(): num users --> '+this.room.users.length);
 			if(this.room.users.length == 0) return;
 			
@@ -90,77 +92,90 @@
 				this.room.vm.play(videoToPlay);					//TODO: need to change meter logic to add points to the suggestor
 				return;
 			} 
-			else if(this.autoPlaylist.getSize() > 0) {
-				console.log('['+roomName+'][VAL] playVideo(): playing a video from the autoplaylist');
-				var videoToPlay = this.autoPlaylist.popVideo();
-				videoToPlay.set({ dj: 'VAL'});
-				this.room.vm.play(videoToPlay);
-				return;
-			} 
-			else {	//fetch related video from YouTube -- this is temporary
-				console.log('['+roomName+'][VAL] playVideo(): no other videos - fetching one from YouTube');
-				if(this.room.history.length == 0) return;
-				
-				var lookBackNum = 3;
-				if (this.room.history.length < lookBackNum) lookBackNum = this.room.history.length;
-				var randInt = Math.floor(Math.random()*lookBackNum + 1);	//between 1 and lookBackNum, inclusive
-				
-				var recentVideo = this.room.history.at(this.room.history.length - randInt);
-				if(!recentVideo) {
-					console.log("ERROR No video found. You should never see this message! ");
-					return;
-				}
-				var videoId = recentVideo.get('videoId');
-				
-				
-				console.log('['+roomName+'][VAL] playVideo(): basing recommendation off of video '+recentVideo.get('title')+", the "+randInt
-					+ "/"+lookBackNum+" most recently played video");
-				
-				var options = { 
-					host: 'gdata.youtube.com',
-					port: 80,
-					path: '/feeds/api/videos/'+videoId+'/related?alt=json&start-index=1&max-results=25&v=2',
-				};
-				
-				var room = this.room;
-				var VAL = this;
-				var req = http.request(options, function(res) {
-				  res.setEncoding('utf8');
-					var videoData = '';
-				  res.on('data', function (chunk) {
-				    videoData += chunk;
-				  });
-				
-					res.on('end', function() {
-						videoData = JSON.parse(videoData);
-						var randIndex = Math.floor(Math.random()*4);	//picks one at random from top 4
-												
-						var videoEntry = videoData['feed']['entry'][randIndex];
-						var videoToPlayId = videoEntry['media$group']['yt$videoid']['$t'];
-						var videoDuration = videoEntry['media$group']['yt$duration']['seconds'];
-						var videoTitle = videoEntry['media$group']['media$title']['$t'];
-						var videoThumb = videoEntry['media$group']['media$thumbnail'][0]['url'];
-						var videoAuthor = videoEntry['author'][0]['name']['$t'];
-		
-						console.log('['+room.get('name')+']'+"[VAL] playVideo(): got related vids, index "+randIndex+"/4 with videoid: "+videoToPlayId+" and title: "+videoTitle);
-						var videoToPlay = new models.Video({
-							videoId: videoToPlayId,
-							duration: videoDuration,
-							title: videoTitle,
-							thumb: videoThumb,
-							author: videoAuthor,
-							dj: 'VAL'
+			else  { 
+				redisClient.llen("room:" + roomName + ":val:playlist", function(err, response) {
+					if(response > 0) {
+						console.log('['+roomName+'][VAL] playVideo(): playing a video from the autoplaylist');
+						redisClient.lpop("room:" + roomName + ":val:playlist", function(err, response) {
+							console.log("LEN AUTOPLAYLIST RESPONSE IS " + response) 
+							var videoToPlay = new models.Video({
+								videoId: response,
+								duration: 10,
+								title: "hi val",
+								thumb: "nothanks",
+								dj: "val"
+							});
+							videoToPlay.set({ dj: 'VAL'});
+							room.vm.play(videoToPlay);
 						});
-						
-						room.vm.play(videoToPlay);
-					});
-				});
-		
-				req.on('error', function(e) {
-				  console.log('['+roomName+'][VAL] playVideo(): *** ERROR *** ' + e.message);
-				});
-				req.end();
-			}
+					
+						return;
+					} 
+					else {	//fetch related video from YouTube -- this is temporary
+						console.log('['+roomName+'][VAL] playVideo(): no other videos - fetching one from YouTube');
+						if(room.history.length == 0) return;
+
+						var lookBackNum = 3;
+						if (room.history.length < lookBackNum) lookBackNum = room.history.length;
+						var randInt = Math.floor(Math.random()*lookBackNum + 1);	//between 1 and lookBackNum, inclusive
+
+						var recentVideo = room.history.at(room.history.length - randInt);
+						if(!recentVideo) {
+							console.log("ERROR No video found. You should never see this message! ");
+							return;
+						}
+						var videoId = recentVideo.get('videoId');
+
+
+						console.log('['+roomName+'][VAL] playVideo(): basing recommendation off of video '+recentVideo.get('title')+", the "+randInt
+							+ "/"+lookBackNum+" most recently played video");
+
+						var options = { 
+							host: 'gdata.youtube.com',
+							port: 80,
+							path: '/feeds/api/videos/'+videoId+'/related?alt=json&start-index=1&max-results=25&v=2',
+						};
+
+
+						var req = http.request(options, function(res) {
+						  res.setEncoding('utf8');
+							var videoData = '';
+						  res.on('data', function (chunk) {
+						    videoData += chunk;
+						  });
+
+							res.on('end', function() {
+								videoData = JSON.parse(videoData);
+								var randIndex = Math.floor(Math.random()*4);	//picks one at random from top 4
+
+								var videoEntry = videoData['feed']['entry'][randIndex];
+								var videoToPlayId = videoEntry['media$group']['yt$videoid']['$t'];
+								var videoDuration = videoEntry['media$group']['yt$duration']['seconds'];
+								var videoTitle = videoEntry['media$group']['media$title']['$t'];
+								var videoThumb = videoEntry['media$group']['media$thumbnail'][0]['url'];
+								var videoAuthor = videoEntry['author'][0]['name']['$t'];
+
+								console.log('['+room.get('name')+']'+"[VAL] playVideo(): got related vids, index "+randIndex+"/4 with videoid: "+videoToPlayId+" and title: "+videoTitle);
+								var videoToPlay = new models.Video({
+									videoId: videoToPlayId,
+									duration: videoDuration,
+									title: videoTitle,
+									thumb: videoThumb,
+									author: videoAuthor,
+									dj: 'VAL'
+								});
+
+								room.vm.play(videoToPlay);
+							});
+						});
+
+						req.on('error', function(e) {
+						  console.log('['+roomName+'][VAL] playVideo(): *** ERROR *** ' + e.message);
+						});
+						req.end();
+					}
+				}); 
+			}	
 		}	
 		
 	});
@@ -233,7 +248,7 @@
 					dj: this.room.currVideo.get('dj')
 				});
 							
-				redisClient.rpush('room:history:' + this.room.get("name"), JSON.stringify(videoFinished));
+				redisClient.rpush('room:' + this.room.get("name") + ':history', JSON.stringify(videoFinished));
 				this.room.history.add(videoFinished);
 				this.room.clearVideo();
 				//TODO: make a call to VAL to use this video to generate more recommendations
@@ -342,6 +357,7 @@
 			roomData.rID = this.get('name');
 			roomData.numDJs = this.djs.length;
 			roomData.numUsers = this.users.length;
+			roomData.history = this.history;
 			if(this.currVideo) roomData.curVidTitle = this.currVideo.get('title');
 			//console.log("Here's the roomdata: " + roomData.rID);
 			return roomData;
