14ba6a97bd0decbe3c468ecbbe1847768efc7878
Anthony Dao <nong.shin@gmail.com>
2011-09-16 20:09:00 -0700

    lol like 5 minute fix to take out Val Vote

--- public/javascripts/gates.js
+++ public/javascripts/gates.js
@@ -1050,9 +1050,9 @@ $(function() {
 
   initialize: function() {
    this.render();
-	 this.valVoteView = new ValVoteView({
-		sideBarView: this
-	 });
+	// this.valVoteView = new ValVoteView({
+	//	sideBarView: this
+	// });
 	 this.videoManagerView = new VideoManagerView({
 		sideBarView: this,
 		searchBarModel: this.options.searchBarModel,
